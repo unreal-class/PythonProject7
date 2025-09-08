759bb68cae817ecf5b527520e038e1e7fd616048
dustinlee@E216-4090-PC <daehyun.lee@gmail.com>
2025-09-08 10:11:28 +0900

    first commit

---
+++ lab_turtle.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.penup()
+turtle.goto(200,200)
+turtle.pendown()
+turtle.circle(50)
+turtle.write("(200,200)")
+
+turtle.penup()
+turtle.goto(200,-200)
+turtle.pendown()
+turtle.circle(30)
+turtle.write("(-200,-200)")
+
+turtle.penup()
+turtle.home()
+turtle.pendown()
+turtle.circle(50)
+turtle.write("Home")
+
+# turtle.bye()
+
